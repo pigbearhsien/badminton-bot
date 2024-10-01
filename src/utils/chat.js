83d6bde65f1ffb7c04e4e58460e1141174e98bcd
sallyhsieh <sallyhsieh@liontrave.com>
2024-10-02 04:02:26 +0800

    add database

--- src/utils/chat.js
+++ src/utils/chat.js
@@ -3,28 +3,18 @@ const { MongoClient } = require("mongodb");
 
 const uri = process.env.MONGO_URI;
 
-const options = {
-  serverSelectionTimeoutMS: 5000, // Adjust as needed
-};
-
 let client;
 let db;
-let clientPromise;
 
 const initDB = async () => {
   if (db) return db; // Return the existing connection if it exists
 
-  if (!clientPromise) {
-    client = new MongoClient(uri, options);
-    clientPromise = client.connect();
-  }
-
-  client = new MongoClient(uri, options);
+  client = new MongoClient(uri);
 
   try {
-    await clientPromise;
+    await client.connect();
     console.log("Connected to MongoDB Atlas");
-    db = client.db("Noah");
+    db = client.db("Noah"); // Replace with your database name
     return db;
   } catch (error) {
     console.error("MongoDB connection error:", error);
@@ -43,6 +33,10 @@ const getConversation = async (userId) => {
 };
 
 const saveConversation = async (userId, messages) => {
+  await initDB();
+  if (!db) {
+    throw new Error("Database not initialized");
+  }
   const collection = db.collection("Chat");
   await collection.updateOne(
     { userId },
